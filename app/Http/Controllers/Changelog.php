c4b92e40c160193867d639bb8ddd2138c2044a4d
Maciej <maciej@grygiel.eu>
2020-03-14 19:38:55 +0100

    Changelog - simple view

--- app/Http/Controllers/Changelog.php
+++ app/Http/Controllers/Changelog.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\ChangelogEntry;
 use App\Document;
 use Illuminate\Http\Request;
 
@@ -9,11 +10,9 @@ class Changelog extends Controller
 {
     public function index()
     {
+        $entries = ChangelogEntry::query()->orderBy('published_at', 'DESC')->where('published', 1)->get();
         $website = app(\Hyn\Tenancy\Environment::class)->tenant();
 
-        dd(Document::all()->first()->number);
-
-        dd($website->id);
-        die();
+        return view('changelog.index', ['entries' => $entries, 'website' => $website]);
     }
 }
---
+++ resources/views/changelog/index.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+
+<body>
+<div class="container-md">
+    @foreach ($entries as $entry)
+    <article>
+        <h2>{{ $entry->title }}</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <h5 style="color: {{ $entry->category->color }}">{{ $entry->category->name }}</h5>
+            </div>
+            <div class="col-md-4">
+                {{ $entry->published_at }}
+            </div>
+        </div>
+        <p>{{ $entry->content }}</p>
+        <hr>
+    </article>
+    @endforeach
+</div>
+</body>
+
+</html>
